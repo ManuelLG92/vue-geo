e4feff90e549038a3eede175fd7e9c35d271e574
manuel.leon <manuel.leon@wefox.com>
2023-05-15 00:06:37 +0200

    feat: Virtualize the front-end

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    // <-- this object is added
+    host: true,
     port: 8000
   },
   plugins: [vue()],
