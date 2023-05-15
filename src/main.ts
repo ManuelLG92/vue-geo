6c3795bd14ecf09e9959917ab9ab26cebe995df7
manuel.leon <manuel.leon@wefox.com>
2023-05-16 01:25:52 +0200

    feat: - Add bootstrap for styles - Delete global state management

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.vue'
 import router from './router'
 import { VueQueryPlugin } from '@tanstack/vue-query'
-
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 app.use(VueQueryPlugin)
 
 app.mount('#app')
+
+import 'bootstrap/dist/js/bootstrap.js'
