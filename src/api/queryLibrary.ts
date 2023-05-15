4dda012c6ca6116c71ef684998ed5e3a44e2f850
manuel.leon <manuel.leon@wefox.com>
2023-05-16 01:34:43 +0200

    feat: - Add cache time

--- src/api/queryLibrary.ts
+++ src/api/queryLibrary.ts
@@ -3,7 +3,8 @@ import { useQuery } from '@tanstack/vue-query'
 const get = (queryKey: string, url: () => Promise<Response>) => {
   return useQuery({
     queryKey: [queryKey],
-    queryFn: url
+    queryFn: url,
+    cacheTime: 300000 // ms
   })
 }
 
